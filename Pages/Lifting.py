3adb196b59f3c4dde66bcec02e1c3adb70b21a36
Rajopriyo Chanda <96880051+rajopriyo@users.noreply.github.com>
2024-05-25 00:46:30 +0530

    Add files via upload

---
+++ Pages/Lifting.py
@@ -0,0 +1,29 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import time
+
+st.set_page_config(
+    page_title="AI Trainer",
+    page_icon="💪",
+)
+st.markdown(
+    """
+    <a href="http://localhost:8501/" >
+        <button style=" background-color: green;
+    border: 4px solid green;
+    border-radius: 10px;
+    color: #bcff96;
+    box-shadow: 10px;
+    padding: 10px 20px ; cursor: pointer;">
+            Go to Home
+        </button>
+    </a>
+""",
+    unsafe_allow_html=True,
+)
+st.write(
+    """
+# Lifting Exercise AI Traier�💪
+"""
+)
